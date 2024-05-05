5ca79878408925ff5b744f18453a6b61f17f978e
Abdullah Mubeen <abdulmubi111@gmail.com>
2024-05-05 16:47:58 +0500

    Added exercise.md in Task folder

---
+++ Task/dist/task11.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Define an arraya with different mode of transportation
+let transportation = ['motorcycle', 'car', 'choper', 'aeroplane', 'wings'];
+// loop through the array to print statment for each element
+for (let transport of transportation) {
+    console.log(`I would like to own a ${transport}`);
+}
---
+++ Task/src/task11.ts
@@ -0,0 +1,7 @@
+// Define an arraya with different mode of transportation
+let transportation: string[] = ['motorcycle', 'car', 'choper', 'aeroplane', 'wings']
+
+// loop through the array to print statment for each element
+for (let transport of transportation) {
+    console.log(`I would like to own a ${transport}`);    
+}
