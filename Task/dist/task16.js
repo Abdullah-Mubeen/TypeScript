d742062aed4edcb2667a75e5f9bf489314dbd562
Abdullah-Mubeen <abdulmubi11@gmail.com>
2024-05-18 15:49:51 +0500

    Added task17 file

--- Task/dist/task16.js
+++ Task/dist/task16.js
@@ -4,11 +4,9 @@ const task12_1 = require("./task12");
 console.log("I'm sorry to inform you that my new dinner table won't arrive in time. I can only invite two guests for dinner.");
 // let invitedGuests = guestList.filter(guest => guest.name.includes("(R.A)"));
 while (task12_1.guestList.length > 2) {
-    let removedGuest = task12_1.guestList.pop();
-    console.log(`${removedGuest}, I'm sorry I can't invite you to dinner.`);
+    let removeGuest = task12_1.guestList.pop();
+    console.log(`${removeGuest?.name}, I'm sorry I can't inite you to dinner.`);
 }
-task12_1.guestList.forEach(guest => {
-    console.log(`${guest}, you're still invited to dinner.`);
+task12_1.guestList.forEach(guests => {
+    console.log(`${guests.name}, the dinner table is for you I am honoured to have you `);
 });
-task12_1.guestList.splice(0, task12_1.guestList.length); // This will empty the array
-console.log(task12_1.guestList); // This should print an empty array: []
---
+++ Task/dist/task17.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const placesToVisit = ["Kashmeer", "Lahore Fort", "Karakoram Highway", "Tomb of Shah Rukn-e-Alam"];
+console.log(`Original order: ${placesToVisit}`);
+const sortedPlaces = placesToVisit.slice().sort();
+console.log(`Sorted array: ${sortedPlaces}`);
+console.log(`Original order: ${placesToVisit}`);
+const reversePlace = placesToVisit.slice().reverse().sort();
+console.log(`Reverse array in alphhabetic order: ${reversePlace}`);
+console.log(`Original order: ${placesToVisit}`);
+placesToVisit.reverse();
+console.log(`Reverse array: ${placesToVisit}`);
+placesToVisit.reverse();
+console.log(`Back to orginal order: ${placesToVisit}`);
